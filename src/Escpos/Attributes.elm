c0913a614f252ea1726e54841fa49493985c6d14
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-16 10:07:26 -0300

    feat: specialized attributes

--- src/Escpos/Attributes.elm
+++ src/Escpos/Attributes.elm
@@ -1,19 +1,29 @@
-module Escpos.Attributes exposing (align, bold, characterSize, underline)
+module Escpos.Attributes exposing (alignCenter, alignLeft, alignRight, bold, normalDouble, normalDoubleHeight, normalDoubleWidth, normalSize, smallDouble, smallDouleHeight, smallDouleWidth, smallSize, underline)
 
 import Escpos.Internal.Model as Internal exposing (Alignment, Attribute, CharacterSizing)
 
 
 
---
+-- ALIGNMENT
 
 
-align : Alignment -> Attribute
-align =
-    Internal.AlignmentAttribute
+alignLeft : Attribute
+alignLeft =
+    Internal.AlignmentAttribute Internal.Left
+
+
+alignCenter : Attribute
+alignCenter =
+    Internal.AlignmentAttribute Internal.Center
+
+
+alignRight : Attribute
+alignRight =
+    Internal.AlignmentAttribute Internal.Left
 
 
 
---
+-- STYLE
 
 
 underline : Attribute
@@ -26,6 +36,45 @@ bold =
     Internal.TextAttribute Internal.Bold
 
 
-characterSize : CharacterSizing -> Attribute
-characterSize =
-    Internal.CharacterSize >> Internal.TextAttribute
+
+-- SIZING
+
+
+normalSize : Attribute
+normalSize =
+    Internal.TextAttribute (Internal.CharacterSize Internal.Normal)
+
+
+normalDoubleWidth : Attribute
+normalDoubleWidth =
+    Internal.TextAttribute (Internal.CharacterSize Internal.NormalDoubleWidth)
+
+
+normalDoubleHeight : Attribute
+normalDoubleHeight =
+    Internal.TextAttribute (Internal.CharacterSize Internal.NormalDoubleHeight)
+
+
+normalDouble : Attribute
+normalDouble =
+    Internal.TextAttribute (Internal.CharacterSize Internal.NormalDouble)
+
+
+smallSize : Attribute
+smallSize =
+    Internal.TextAttribute (Internal.CharacterSize Internal.Small)
+
+
+smallDouleWidth : Attribute
+smallDouleWidth =
+    Internal.TextAttribute (Internal.CharacterSize Internal.SmallDoubleWidth)
+
+
+smallDouleHeight : Attribute
+smallDouleHeight =
+    Internal.TextAttribute (Internal.CharacterSize Internal.SmallDoubleHeight)
+
+
+smallDouble : Attribute
+smallDouble =
+    Internal.TextAttribute (Internal.CharacterSize Internal.SmallDouble)
